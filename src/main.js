9bbe464c1a39fc006f521502cc61f326af3fdc59
xuyangyang2019 <15268296911@163.com>
2020-08-24 15:03:49 +0800

    实现ssr的小demo

--- src/main.js
+++ src/main.js
@@ -1,33 +1,16 @@
+// main.js
 import Vue from "vue"
 import App from "./App.vue"
-import router from "./router"
-import store from "./store"
+import { createRouter } from "./router"
 
-// 导入api接口
-import api from "./api"
-import filters from "./filters"
-// 自定义的toast
-import Toast from "./components/toast/index"
-// 导入icon-font
-import "@/assets/font/iconfont.js"
-
-Vue.config.productionTip = false
-// 将api挂载到vue的原型上复制代码
-Vue.prototype.$api = api
-Vue.use(Toast)
-// Vue.prototype.$toast = Toast
-
-// 全局过滤器
-Object.keys(filters).forEach((filterName) => {
-  Vue.filter(filterName, filters[filterName])
-})
-
-// 选择高亮的css 通过babel配置
-// prism-coy prism-dark prism-funky prism-okaidia prism-tomorrow prism-twilight
-// import "prismjs/themes/prism-okaidia.css"
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App)
-}).$mount("#app")
+// 导出一个工厂函数，用于创建新的
+// 应用程序、router 和 store 实例
+export function createApp() {
+  const router = createRouter()
+  const app = new Vue({
+    router,
+    // 根实例简单的渲染应用程序组件。
+    render: (h) => h(App)
+  })
+  return { app, router }
+}
