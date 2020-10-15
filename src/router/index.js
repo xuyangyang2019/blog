e0889702333b739a9454aa28f45319876b14a61d
xuyangyang2019 <15268296911@163.com>
2020-10-15 18:33:30 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,19 +7,70 @@ Vue.use(Router)
 
 function createRouter() {
     const routes = [
+        // {
+        //     path: "*",
+        //     name: "miss",
+        //     component: miss
+        // },
         {
-            path: '/',
-            redirect: '/home'
+            path: "/",
+            redirect: "/home"
         },
         {
-            path: '/home',
-            name: 'home',
-            component: Home
+            path: "/home",
+            name: "home",
+            component: Home,
         },
         {
-            path: '/foo',
-            component: () => import(/* webpackChunkName: "foo" */ '../components/Foo.vue') // 异步路由
-        }
+            path: "/article",
+            name: "article",
+            component: () => import(/* webpackChunkName: "article" */ '../views/ArticleRoot.vue') // 异步路由
+        },
+        // article的子路由
+        // {
+        //     path: "/article/:articleList",
+        //     name: "techincal",
+        //     component: techincal
+        // },
+        // {
+        //     path: "/article/:articleList/:id",
+        //     name: "articleShow",
+        //     component: articleShow
+        // },
+        {
+            path: "/life",
+            name: "life",
+            component: () => import(/* webpackChunkName: "life" */ '../views/Life.vue') // 异步路由
+        },
+        // {
+        //     path: "/life/:id",
+        //     name: "lifeShow",
+        //     component: articleShow
+        // },
+        {
+            path: "/msgboard",
+            name: "msgboard",
+            component: () => import(/* webpackChunkName: "msgboard" */ '../views/MsgBoard.vue') // 异步路由
+        },
+        // {
+        //     path: "/search/:searchKey",
+        //     name: "search",
+        //     component: search
+        // },
+        // {
+        //     path: "/timeLine/:time",
+        //     name: "timeLine",
+        //     component: timeLine
+        // },
+        // {
+        //     path: "/login_github",
+        //     name: "loginGithub",
+        //     component: loginGithub
+        // }
+        // {
+        //     path: '/foo',
+        //     component: () => import(/* webpackChunkName: "foo" */ '../components/Foo.vue') // 异步路由
+        // }
     ]
 
     const router = new Router({
