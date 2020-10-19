767b45c33bcf52e543d369d918424bcdc986cbc3
xuyangyang2019 <15268296911@163.com>
2020-10-19 18:41:06 +0800

    展示标签

--- src/components/base/GateWay.vue
+++ src/components/base/GateWay.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="gateway">
-    <h2 class="gateway-header">标签</h2>
+    <h2 class="gateway-header" @click="getTagsClass">标签</h2>
     <div class="gateway-content">
       <ul>
         <li v-for="item in tags">
-          <a href="javascript: void(0)" @click="jumpGate(item.tag)" ref="tag">{{ item.tag | changeLife }}</a>
+          <!-- <a href="javascript: void(0)" @click="jumpGate(item.tag)" ref="tag">{{ item.tag | changeLife }}</a> -->
+          <a href="javascript: void(0)" @click="jumpGate(item.tag)" ref="tag">{{ item }}</a>
         </li>
       </ul>
     </div>
@@ -17,7 +18,6 @@ import { mapState, mapMutations, mapActions } from "vuex"
 export default {
   data() {
     return {
-      tags: [],
       color: ["#FF9933", "#663300", "#CC6600", "#99CC33", "#CC6699", "#009966", "#999999", "#336666", "#9BBFEA", "#CCCC00"]
     }
   },
@@ -30,35 +30,48 @@ export default {
       }
     }
   },
-  mounted() {
-    let that = this
-    // this.getTagsclass({ publish: true }).then((data) => {
-    //   //没有标签则不要初始化，否则会出错
-    //   if (data.length) {
-    //     that.initBackground()
-    //   }
-    // })
-  },
   computed: {
-    // ...mapState(["tags"])
+    ...mapState({
+      tags: 'tags'
+    })
+  },
+  watch: {
+    tags() {
+      this.initBackground()
+    }
   },
   methods: {
-    ...mapActions(["getTagsclass"]),
+    ...mapActions({
+      getTagsClass: 'GetTagsClass'
+    }),
+    // 初始化背景色
     initBackground: function () {
       this.$nextTick(() => {
-        //IE10不支持refs.tag
+        // IE10不支持refs.tag
         this.$refs.tag.forEach((item, index, arr) => {
           item.style.backgroundColor = this.color[Math.floor(Math.random() * 10)]
         })
       })
     },
-    jumpGate: function (item) {
-      if (item === "life") {
-        this.$router.push({ name: "life" })
-      } else {
-        this.$router.push({ name: "techincal", params: { articleList: item } })
-      }
+    // 跳转到标签
+    jumpGate(item) {
+      console.log(itme)
+      // if (item === "life") {
+      //   this.$router.push({ name: "life" })
+      // } else {
+      //   this.$router.push({ name: "techincal", params: { articleList: item } })
+      // }
     }
+  },
+  mounted() {
+    this.getTagsClass({ publish: true })
+    // this.getTagsClass({ publish: true }).then((data) => {
+    //   //没有标签则不要初始化，否则会出错
+    //   console.log(data)
+    //   if (data.length) {
+    //     this.initBackground()
+    //   }
+    // })
   }
 }
 </script>
