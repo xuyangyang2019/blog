9e97c184bd2d4cc74e45d1fea18c35e52716fd35
xuyangyang2019 <15268296911@163.com>
2020-10-20 11:43:17 +0800

    获取时间轴

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -55,6 +55,35 @@ module.exports = {
         })
         ctx.body = tagArr
     },
+    // 获取时间轴
+    'GET /api/getTime': async (ctx, next) => {
+        let publish = ctx.query.publish === "false" ? false : true
+        let timeArr = []
+        let doc = await db.article.find(
+            { publish: publish }, // 查询条件
+            { date: 1 }, // 指定返回的字段
+            // (err, doc) => {
+            //     if (err) {
+            //         console.log('查询数据库报错', err)
+            //     }
+            // }
+        )
+        let timeMap = {}
+        doc.forEach((item, index, arr) => {
+            let yearMonth = new Date(item.date).getFullYear() + "年" + (new Date(item.date).getMonth() + 1) + "月"
+            timeMap[yearMonth] = timeMap[yearMonth] ? timeMap[yearMonth] + 1 : 1
+        })
+        for (const key in timeMap) {
+            if (timeMap.hasOwnProperty(key)) {
+                timeArr.push({
+                    time: key,
+                    num: timeMap[key]
+                })
+            }
+        }
+        console.log('getTime', timeArr)
+        ctx.body = timeArr
+    },
     // 'GET /api/admins': async (ctx, next) => {
     //     await adminService.getList(ctx)
     //     // adminModel.find({}, {}, (err, docs) => {
