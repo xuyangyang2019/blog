a8f8e01265e4e9deb16a745540071c163fb290be
xuyangyang2019 <15268296911@163.com>
2020-10-20 19:36:32 +0800

    首页展示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,30 +3,53 @@
     <banner></banner>
     <loading v-if="code === 404"></loading>
     <h3 v-if="articles.all.length === 0 && code === 200" class="none-article">还没有文章，敬请期待···</h3>
-    <!-- <article-list :articleList="articles.all"></article-list> -->
+    <article-list :articleList="articles.all"></article-list>
   </div>
 </template>
 <script>
-// import { mapActions, mapState, mapMutations } from "vuex"
+import { mapActions, mapState, mapMutations } from "vuex"
 
 import banner from "@/components/home/Banner"
 import loading from "@/components/base/Loading"
-// import articleList from "@/components/article/ArticleList"
-import articleList from "../components/article/ArticleList"
+import articleList from "@/components/article/ArticleList"
 
 export default {
-  data() {
-    return {
-      articles: {
-        all: []
-      },
-      code: 200
-    }
+  asyncData({ store, route }) {
+    return Promise.all([
+      store.dispatch("GetArticles", {
+        publish: true,
+        page: 1,
+        cache: true
+      }),
+      store.dispatch("GetArticlesCount", {
+        cache: true,
+        publish: true
+      })
+      // .then(() => {
+      //   store.commit("CHANGE_CODE", 200)
+      // })
+    ])
   },
   components: {
     banner,
     loading,
-    // articleList,
+    articleList,
+  },
+  computed: {
+    ...mapState({
+      code: 'code',
+      articles: 'articles',
+    })
+  },
+  methods: {
+    ...mapActions({
+      getArticles: 'GetArticles',
+      getArticlesCount: 'GetArticlesCount'
+    }),
+    ...mapMutations({
+      clear: 'CLEAR_PAGE',
+      // changeCode: 'CHANGE_CODE'
+    })
   },
   metaInfo() {
     return {
@@ -34,37 +57,15 @@ export default {
       meta: [{ vmid: "description", name: "description", content: "首页 -mapblog小站" }]
     }
   },
-  //   asyncData({ store, route }) {
-  //     return Promise.all([
-  //       store.dispatch("getArticles", {
-  //         publish: true,
-  //         page: 1,
-  //         cache: true
-  //       }),
-  //       store.dispatch("getArticlesCount", {
-  //         cache: true,
-  //         publish: true
-  //       }).then(() => {
-  //         store.commit("changeCode", 200)
-  //       })
-  //     ])
-  //   },
-  //   beforeRouteLeave(to, from, next) {
-  //     this.clear()
-  //     next()
-  //   },
+  beforeRouteLeave(to, from, next) {
+    this.clear()
+    next()
+  },
   mounted() {
     // this.$nextTick(function () {
     //   Prism.highlightAll()
     // })
   },
-  computed: {
-    // ...mapState(["articles", "code"]),
-  },
-  methods: {
-    // ...mapActions(["getArticlesCount"]),
-    // ...mapMutations(["clear", "changeCode"])
-  }
 }
 </script>
 <style lang = "scss" scoped>
