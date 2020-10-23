2c3e834918c02e1c9d20dfdac20f05baad147dac
xuyangyang2019 <15268296911@163.com>
2020-10-23 18:12:37 +0800

    保存留言并创建新的信息

--- server/api/msgBoard.js
+++ server/api/msgBoard.js
@@ -1,5 +1,5 @@
 const db = require("../db/mongodb/db")
-// const localTime = require("../utils/reviseTime")
+const localTime = require("../utils/reviseTime")
 // const confirmToken = require("../middleware/confirmToken")
 
 module.exports = {
@@ -13,5 +13,26 @@ module.exports = {
 			.skip(skip)
 			.limit(limit)
 		ctx.body = docs
+	},
+	// 保存留言
+	'POST /saveLeaveW': async (ctx, next) => {
+		let doc = ctx.request.body
+		let result = await db.msgBoard.create(doc)
+		if (result._id) {
+			ctx.body = result
+			// 保存到新消息 提醒后台
+			db.newMsg.create({
+				type: "msgboard",
+				name: result.name,
+				say: result.content,
+				content: result.name + "在" + localTime(Date.now()) + "给你留言啦~~"
+			})
+			// 		new db.newMsg({
+			// 			type: "msgboard",
+			// 			name: req.body.name,
+			// 			say: req.body.content,
+			// 			content: req.body.name + "在" + localTime(Date.now()) + "给你留言啦~~"
+			// 		}).save()
+		}
 	},
 }
