34937716e05731a1a1608db943722cfd9ced0ba0
xuyangyang2019 <15268296911@163.com>
2020-10-27 17:43:27 +0800

    head minxin

---
+++ src/mixins/head-mixin.js
@@ -0,0 +1,32 @@
+function getHead(vm) {
+    const { head } = vm.$options
+    if (head) {
+        return typeof head === 'function' ? head.call(vm) : head
+    }
+}
+
+const serverHeadMixin = {
+    created() {
+        const head = getHead(this)
+        if (head) {
+            if (head.title) this.$ssrContext.title = `${head.title}`
+            if (head.author) this.$ssrContext.author = `${head.author}`
+            if (head.keywords) this.$ssrContext.keywords = head.keywords
+            if (head.description) this.$ssrContext.description = head.description
+        }
+    }
+}
+
+const clientHeadMixin = {
+    mounted() {
+        const head = getHead(this)
+        if (head) {
+            if (head.title) document.title = `${head.title}`
+            if (head.author) document.querySelector('meta[name="author"]').setAttribute('content', `${head.author}`)
+            if (head.keywords) document.querySelector('meta[name="keywords"]').setAttribute('content', head.keywords)
+            if (head.description) document.querySelector('meta[name="description"]').setAttribute('content', head.description)
+        }
+    }
+}
+
+export default process.env.VUE_ENV === 'server' ? serverHeadMixin : clientHeadMixin
