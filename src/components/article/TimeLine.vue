dd7df2a829679ace68f84730584e090fe20c8387
xuyangyang2019 <15268296911@163.com>
2020-10-30 10:06:11 +0800

    展示归档条件的文章

---
+++ src/components/article/TimeLine.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="timeLine">
+    <loading v-if="articles.time.length === 0"></loading>
+    <article-list :articleList="articles.time"></article-list>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapState, mapMutations } from "vuex"
+
+import articleList from "@/components/article/ArticleList"
+import loading from "@/components/base/Loading"
+
+export default {
+  getHead() {
+    return {
+      title: "时间轴 -mapblog小站"
+    }
+  },
+  components: {
+    articleList,
+    loading
+  },
+  computed: {
+    ...mapState({
+      articles: 'articles'
+    })
+  },
+  watch: {
+    $route() {
+      this.time_arts()
+    }
+  },
+  methods: {
+    ...mapActions({
+      getArticlesCount: 'GetArticlesCount',
+      timeArticles: 'TimeArticles'
+    }),
+    ...mapMutations({
+      clear: 'CLEAR_PAGE'
+    }),
+    time_arts() {
+	  let timeArr = this.$route.params.time.match(/\d+\-\d+\-\d+/g)
+      // utc时间0点起
+      let startTime = new Date(Date.parse(timeArr[0])).getTime()
+      // utc时间24点
+      let endTime = new Date(Date.parse(timeArr[1])).getTime() + 1000 * 60 * 60 * 24
+      let params = {
+        publish: true,
+        page: 1,
+        start: startTime,
+        end: endTime,
+        according: "time"
+      }
+      this.timeArticles(params)
+      this.getArticlesCount(params)
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    this.clear()
+    next()
+  },
+  created() {
+    this.time_arts()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
