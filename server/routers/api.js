586324cb2d56919e44d24e5cb8a3a4c290d8ca20
xuyangyang2019 <15268296911@163.com>
2020-11-04 15:45:45 +0800

    admin相关的路由鉴权

--- server/routers/api.js
+++ server/routers/api.js
@@ -1,5 +1,7 @@
 const fs = require('fs')
 const path = require('path')
+const confirmToken = require("../middlewares/confirmToken")
+
 
 /**
  * 绑定路由
@@ -7,27 +9,27 @@ const path = require('path')
  * @param {*} mapping 要绑定的方法{路由path:方法,...}
  */
 function addMapping(router, mapping) {
-  for (var url in mapping) {
+  for (const url in mapping) {
     if (url.startsWith('GET ')) {
-      var path = url.substring(4)
-      router.get(path, mapping[url])
-      // console.log(`register URL mapping: GET ${path}`)
+      let path = url.substring(4)
+      // 权限校验
+      if (path.indexOf('getAdmin') === 0) {
+        router.get(path, confirmToken, mapping[url])
+      } else {
+        router.get(path, mapping[url])
+      }
     } else if (url.startsWith('POST ')) {
-      var path = url.substring(5)
+      let path = url.substring(5)
       router.post(path, mapping[url])
-      // console.log(`register URL mapping: POST ${path}`)
     } else if (url.startsWith('PUT ')) {
-      var path = url.substring(4)
+      let path = url.substring(4)
       router.put(path, mapping[url])
-      // console.log(`register URL mapping: PUT ${path}`)
     } else if (url.startsWith('PATCH ')) {
-      var path = url.substring(6)
+      let path = url.substring(6)
       router.patch(path, mapping[url])
-      // console.log(`register URL mapping: DELETE ${path}`)
     } else if (url.startsWith('DELETE ')) {
-      var path = url.substring(7)
+      let path = url.substring(7)
       router.del(path, mapping[url])
-      // console.log(`register URL mapping: DELETE ${path}`)
     } else {
       console.log(`invalid URL: ${url}`)
     }
@@ -43,17 +45,17 @@ function addControllers(router) {
   // console.log('addControllers')
   // 先导入fs模块，然后用readdirSync列出文件
   // 这里可以用sync是因为启动时只运行一次，不存在性能问题:
-  // var files = fs.readdirSync(__dirname + `/${controllers_dir}`)
+  // let files = fs.readdirSync(__dirname + `/${controllers_dir}`)
   let controllers_path = path.resolve(__dirname, '../api')
   // console.log(controllers_path)
-  var files = fs.readdirSync(controllers_path)
+  let files = fs.readdirSync(controllers_path)
   // 过滤出.js文件:
-  var js_files = files.filter((f) => {
+  let js_files = files.filter((f) => {
     return f.endsWith('.js')
   })
 
   // 处理每个js文件:
-  for (var f of js_files) {
+  for (let f of js_files) {
     // console.log(`process controller: ${f}...`)
     // 导入js文件
     let mapping = require(`${controllers_path}/` + f)
