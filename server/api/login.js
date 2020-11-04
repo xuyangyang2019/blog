f6e5779317ec7c5f3f1b45b1c9c9db89e39b47cd
xuyangyang2019 <15268296911@163.com>
2020-11-04 11:39:33 +0800

    后端登陆接口

---
+++ server/api/login.js
@@ -0,0 +1,59 @@
+const md5 = require("md5")
+const jwt = require("jsonwebtoken")
+
+const secret = require("../secret")
+const db = require("../db/mongodb/db")
+const localTime = require("../utils/reviseTime")
+// const confirmToken = require("../middlewares/confirmToken")
+
+// 创建token
+const createToken = (id, name) => {
+	return jwt.sign({ id: id, user: name }, secret.jwtSecret, { expiresIn: "10h" })
+}
+
+// //路由闯入编辑器页面进行token验证
+// router.get("/api/confirmToken", confirmToken, (ctx, res) => {
+// 	res.status(200).end()
+// })
+
+module.exports = {
+	// 后端登陆
+	'POST /login': async (ctx, next) => {
+		let doc = await db.user.findOne({ user: ctx.request.body.user })
+		if (doc && doc._id && doc.salt) {
+			// 如果有用户且密码正确 返回token
+			if (doc.password === md5(ctx.request.body.password + doc.salt)) {
+				// 生成token
+				let token = createToken(doc._id, doc.user)
+				// 上次登陆时间
+				let lastTime = doc.lastLogin
+				// 当前时间
+				let currTime = localTime(Date.now())
+				// 更新用户表的最近一次的登陆时间
+				db.user.update(
+					{ user: ctx.request.body.user },
+					{ lastLogin: currTime }, (err, doc) => {
+						if (err) {
+							console.log(err)
+						}
+					})
+				// 返回
+				ctx.body = {
+					code: 200,
+					id: doc._id,
+					name: doc.user,
+					lastLogin: lastTime,
+					token: token
+				}
+
+			} else {
+				// 如果密码不对 返回401
+				ctx.body = { code: 401 }
+			}
+		} else {
+			// 如果查不到用户名 返回401
+			ctx.body = { code: 401 }
+		}
+	},
+
+}
