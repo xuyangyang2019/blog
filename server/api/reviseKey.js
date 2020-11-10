5fe0ee658785602de59464b74e4a26b54d15c24b
xuyangyang2019 <15268296911@163.com>
2020-11-10 17:10:36 +0800

    修改密码

---
+++ server/api/reviseKey.js
@@ -0,0 +1,26 @@
+const db = require("../db/mongodb/db")
+// const md5 = require("js-md5")
+const md5 = require("md5")
+const secret = require("../secret")
+
+const confirmToken = require("../middlewares/confirmToken")
+
+module.exports = {
+  // 修改密码
+  'PATCH /reviseKey': async (ctx, next) => {
+    confirmToken(ctx, next)
+    let salt = secret.salt
+    let pwd = md5(ctx.request.body.newKey + salt)
+    let doc = await db.user.findOne({ user: "admin" })
+    if (doc) {
+      if (doc.password === md5(ctx.request.body.oldKey + doc.salt)) {
+        let result = await db.user.update({ user: "admin" }, { password: pwd, salt: salt })
+        if (result.ok) {
+          ctx.body = ({ code: 200 })
+        }
+      } else {
+        ctx.body = { code: "oldkey-401" }
+      }
+    }
+  }
+}
