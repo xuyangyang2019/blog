519b4d666aff3f8bb2c00901ada2ebdc7b40b922
xuyangyang2019 <15268296911@163.com>
2020-11-11 16:18:20 +0800

    添加注释

--- server/utils/deleteF.js
+++ server/utils/deleteF.js
@@ -1,48 +1,47 @@
 const fs = require("fs")
 const path = require("path")
+
+// 删除文件夹
 function deleteFolder(url) {
-    let files = []
-    //判断给定的路径是否存在
-    if( fs.existsSync(url) ) {
-        //返回文件和子目录的数组
-        files = fs.readdirSync(url)
-        files.forEach(function(file,index){
-            // var curPath = url + "/" + file
-            let curPath = path.join(url,file)
-            //fs.statSync同步读取文件夹文件，如果是文件夹，在重复触发函数
-            if(fs.statSync(curPath).isDirectory()) { // recurse
+    // 判断给定的路径是否存在
+    if (fs.existsSync(url)) {
+        // 返回文件和子目录的数组
+        let files = fs.readdirSync(url)
+        files.forEach(function (file, index) {
+            let curPath = path.join(url, file)
+            if (fs.statSync(curPath).isDirectory()) {
+                // fs.statSync同步读取文件夹文件，如果是文件夹，在重复触发函数
+                console.log('含有子文件夹', file)
                 deleteFolder(curPath)
-            // 是文件delete file
-            }else{
+            } else {
+                // 是文件delete file
                 fs.unlinkSync(curPath)
             }
         })
-        //清除文件夹
+        // 清除文件夹
         fs.rmdirSync(url)
-    }else{
-        console.log("给定的路径不存在，请给出正确的路径")
     }
 }
+
+// 删除文件
 function deleteFile(url) {
-    let files = []
-    //判断给定的路径是否存在
-    if( fs.existsSync(url) ) {
-        //返回文件和子目录的数组
-        files = fs.readdirSync(url);
-        files.forEach(function(file,index){
-            let curPath = path.join(url,file)
-            //fs.statSync同步读取文件夹文件，如果是文件夹，重复触发函数
-            if(fs.statSync(curPath).isDirectory()) {
+    // 判断给定的路径是否存在
+    if (fs.existsSync(url)) {
+        // 返回文件和子目录的数组
+        let files = fs.readdirSync(url)
+        files.forEach(function (file, index) {
+            let curPath = path.join(url, file)
+            if (fs.statSync(curPath).isDirectory()) {
+                // fs.statSync同步读取文件夹文件，如果是文件夹，重复触发函数
                 deleteFile(curPath)
-            // 是文件delete file
-            }else{
-                fs.unlinkSync(curPath);
+            } else {
+                // 是文件delete file
+                fs.unlinkSync(curPath)
             }
         })
-    }else{
-        console.log("给定的路径不存在，请给出正确的路径")
     }
 }
+
 const deleteF = {
     folder: deleteFolder,
     file: deleteFile
