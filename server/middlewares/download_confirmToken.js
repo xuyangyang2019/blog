4a9ea7e5f570dd8b24021b9b940052b04523fed7
xuyangyang2019 <15268296911@163.com>
2020-11-12 16:19:49 +0800

    下载请求 鉴权

--- server/middlewares/download_confirmToken.js
+++ server/middlewares/download_confirmToken.js
@@ -1,16 +1,17 @@
 /*下载请求是在a标签中发起的，所以token在请求参数中带上，故重写token验证函数*/
+
 const jwt = require("jsonwebtoken")
 const secret = require("../secret")
 
-const download_confirmToken = (req,res,next) => {
-	if(!req.query.authToken){
-		res.status(401).end()
-	}else{
-		const token = req.query.authToken
-		jwt.verify(token,secret.jwtSecret,(err) => {
-			if(err){
-				res.status(401).end()
-			}else{
+const download_confirmToken = (ctx, next) => {
+	if (!ctx.query.authToken) {
+		ctx.status = 401
+	} else {
+		const token = ctx.query.authToken
+		jwt.verify(token, secret.jwtSecret, (err) => {
+			if (err) {
+				ctx.status = 401
+			} else {
 				console.log("通过")
 				next()
 			}
