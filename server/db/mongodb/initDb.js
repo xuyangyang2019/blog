cfc4cfbc341e910c3bc7261c669b41d41f468dab
xuyangyang2019 <15268296911@163.com>
2020-11-16 17:02:14 +0800

    数据初始化 添加一个admin

---
+++ server/db/mongodb/initDb.js
@@ -0,0 +1,26 @@
+const md5 = require('md5')
+const salt = require('../../secret').salt
+const User = require('../../models/mongodb/UserModel')
+const localTime = require("../../utils/reviseTime")
+
+User.find({}, (err, docs) => {
+    if (err) {
+        console.log(err)
+    } else {
+        if (!docs.length) {
+            const user = new User({
+                user: "admin",
+                password: md5("123456" + salt),
+                salt: salt,
+                lastLogin: localTime(Date.now()),
+            })
+            user.save((err, newUser) => {
+                if (err) return console.error(err)
+                console.log(newUser)
+                console.log('插入数据成功，按 ctrl + c 退出！')
+            })
+        } else {
+            console.log("Userinit has done")
+        }
+    }
+})
