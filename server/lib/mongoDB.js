bdf5989b56f5c2a4e6a20dd717fd5b68b249d98a
xuyangyang2019 <15268296911@163.com>
2020-11-17 11:49:14 +0800

    mongoDB配置

---
+++ server/lib/mongoDB.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const config = require('../config').mongoDB
+const { logger } = require('../middlewares/logger')
+
+const url = `mongodb://${config.user}:${config.pwd}@${config.host}:${config.port}/${config.db}?authSource=${config.authSource}`
+// 创建一个数据库连接
+const mongo = mongoose.createConnection(url, { useNewUrlParser: true, useUnifiedTopology: true }) 
+
+
+mongo.on('connected', () => logger.info(`Connected to database: ${url}`))
+
+mongo.on('error', () => logger.error(`Failed to connect to database: ${url}`))
+
+mongo.on('disconnected', () => logger.info(`Closed connection to database: ${url}`))
+
+mongo.once('open', () => logger.info("MongoDB is opened"))
+
+module.exports = mongo
