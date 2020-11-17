8ab48e173ced95388a19f6c4c263b99c9a24014f
xuyangyang2019 <15268296911@163.com>
2020-11-17 11:49:31 +0800

    公共配置文件

---
+++ server/config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  // port: '10081',
+  tokenConfig: {
+    secret: 'havana',
+    expired: 60 * 60 * 24 //1h 
+  },
+  // publicDir: path.resolve(__dirname, './public'),
+  // viewsDir: path.resolve(__dirname, './views'),
+  logPath: path.resolve(__dirname, './logs/koa-server'),
+  mongoDB: {
+    host: '127.0.0.1',
+    port: 27017,
+    user: 'xyy',
+    pwd: '123456',
+    db: 'blog',
+    authSource: 'admin',
+  }
+}
