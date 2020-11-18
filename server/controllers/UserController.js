c9106a19f52ade38ad63089af5ddd82a8cdf3317
xuyangyang2019 <15268296911@163.com>
2020-11-18 15:28:30 +0800

    管理员登陆

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,10 +1,14 @@
+const md5 = require("md5")
 const jwt = require('jsonwebtoken')
-const tokenConfig = require('../config').tokenConfig
+const secret = require("../secret")
+const localTime = require("../utils/reviseTime")
+
 const UserService = require('../services').UserService
 const { InvalidQueryError } = require('../lib/error')
 
+
 module.exports = {
-  'POST /api/login': async (ctx, next) => {
+  'POST /api/adminLogin': async (ctx, next) => {
     const { username, password } = ctx.request.body
     if (!username || !password) {
       throw new InvalidQueryError()
@@ -13,46 +17,28 @@ module.exports = {
     if (!user) {
       ctx.error = '用户不存在'
       ctx.code = 0
-    } else if (user.password !== password) {
+    } else if (user.password !== md5(password + user.salt)) {
       ctx.error = '密码错误'
     } else {
+      // 其他方式生成token
+      // jwt.sign({ id: user._id, username: user.user.username }, secret.jwtSecret, { expiresIn: "10h" })
       ctx.result = {
         userInfo: {
           id: user._id,
           username: user.username,
           nickname: user.nickname,
+          last_login_time: user.last_login_time,
         },
-        token: jwt.sign({
-          data: user._id,
-          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3), //设置 token 过期时间: 3d
-        }, tokenConfig.secret)
+        token:
+          jwt.sign({
+            data: user._id,
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 3) // 3个小时 
+          }, secret.jwtSecret)
       }
-    }
 
-    return next()
-  },
-  'POST /api/register': async (ctx, next) => {
-    const { username, password } = ctx.request.body
-    if (!username || !password) {
-      throw new InvalidQueryError()
+      // 更新用户表的最近一次的登陆时间
+      UserService.updateById(user._id, { last_login_time: localTime(Date.now()) })
     }
-    if (await UserService.findOne({ username })) {
-      ctx.error = '用户已存在'
-    } else {
-      const user = UserService.save({ username, password })
-      ctx.result = {
-        userInfo: {
-          id: user._id,
-          username: user.username,
-          nickname: user.nickname,
-        },
-        token: jwt.sign({
-          data: user._id,
-          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3), //设置 token 过期时间: 3d
-        }, tokenConfig.secret)
-      }
-    }
-
     return next()
   }
 }
