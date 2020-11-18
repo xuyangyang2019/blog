45f63315366e346fb482f02fae95f0c56bb488d2
xuyangyang2019 <15268296911@163.com>
2020-11-18 11:07:58 +0800

    初始化数据库 添加一个admin

--- server/db/mongodb/initDb.js
+++ server/db/mongodb/initDb.js
@@ -2,26 +2,28 @@ const md5 = require('md5')
 const salt = require('../../secret').salt
 const localTime = require("../../utils/reviseTime")
 
-const User = require('../../models/mongodb/UserModel')
+const UserService = require('../../services').UserService
 
-User.find({}, (err, docs) => {
-    if (err) {
-        console.log(err)
-    } else {
-        if (!docs.length) {
-            const user = new User({
-                user: "admin",
-                password: md5("123456" + salt),
-                salt: salt,
-                lastLogin: localTime(Date.now()),
-            })
-            user.save((err, newUser) => {
-                if (err) return console.error(err)
-                console.log(newUser)
-                console.log('插入数据成功，按 ctrl + c 退出！')
-            })
+async function initUser() {
+    let users = await UserService.findMany({})
+    if (users.length === 0) {
+        let user = await UserService.save({
+            username: 'admin', // 用户名
+            password: md5("123456" + salt), // 密码
+            nickname: '羊羊羊', // 昵称
+            phone_num: '10086', // 电话号码
+            salt: salt, // 盐
+            last_login_time: localTime(Date.now()), // 上次登录时间
+        })
+        if (user._id) {
+            console.log(user)
+            console.log('插入数据成功，按 ctrl + c 退出！')
         } else {
-            console.log("Userinit has done")
+            console.log('添加admin失败！')
         }
+    } else {
+        console.log("Userinit has done", users)
     }
-})
+}
+
+initUser()
