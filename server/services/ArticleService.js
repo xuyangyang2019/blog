05269a1040efb032df118b6a9ce80300ec228d87
xuyangyang2019 <15268296911@163.com>
2020-11-19 16:43:48 +0800

    添加两个方法

--- server/services/ArticleService.js
+++ server/services/ArticleService.js
@@ -5,6 +5,21 @@ class ArticleService extends BaseDAO {
   constructor(model) {
     super(model)
   }
+
+  async getArticlesByPv() {
+    const result = await this.model
+      .find(
+        { publish: true },
+        { title: 1, articleId: 1, tag: 1 },
+        { sort: { pv: -1 } })
+      .limit(5)
+    return result
+  }
+
+  async getArticlesCount(condition) {
+    const count = await this.model.countDocuments(condition)
+    return count
+  }
 }
 
 module.exports = new ArticleService(Article)
