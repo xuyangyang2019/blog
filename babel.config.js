6cb74284559e9928c277990fe84db3ac6fa023f2
xuyangyang2019 <15268296911@163.com>
2021-01-08 19:27:40 +0800

    babel配置文件

---
+++ babel.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env'
+    // '@vue/cli-plugin-babel/preset'
+  ],
+  'plugins': [
+    'dynamic-import-webpack',
+    ['prismjs',
+      {
+        'languages': ['html', 'css', 'javascript', 'php', 'dart', 'bash', 'nginx', 'sql'],
+        'plugins': ['line-numbers', 'highlight-keywords'],
+        'theme': 'okaidia',
+        'css': true
+      }]
+  ]
+  // "presets": [
+  //     '@vue/cli-plugin-babel/preset'
+  // ],
+  // "plugins": [
+  //     [
+  //         "component",
+  //         {
+  //             "libraryName": "element-ui",
+  //             "styleLibraryName": "theme-chalk"
+  //         }
+  //     ]
+  // ]
+}
