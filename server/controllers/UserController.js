6f491585af57a97938d30e67a2cb0851444d19e5
xuyangyang2019 <15268296911@163.com>
2021-01-14 14:47:23 +0800

    代码优化

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,10 +3,26 @@ const jwt = require('jsonwebtoken')
 const secret = require('../secret')
 const localTime = require('../utils/reviseTime')
 
-const UserService = require('../services').UserService
 const { InvalidQueryError } = require('../lib/error')
 
+const UserService = require('../services').UserService
+
+// 创建token
+const createToken = (user) => {
+  // 方式一
+  // return jwt.sign({ id: user._id, username: user.user.username }, secret.jwtSecret, { expiresIn: "10h" })
+  // 方式二
+  return jwt.sign(
+    {
+      data: user._id,
+      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 3 // 3个小时
+    },
+    secret.jwtSecret
+  )
+}
+
 module.exports = {
+  // 后端登陆
   'POST /api/login': async (ctx, next) => {
     const { username, password } = ctx.request.body
     // 参数不对抛出异常
@@ -20,7 +36,6 @@ module.exports = {
       ctx.error = '密码错误'
     } else {
       // 其他方式生成token
-      // jwt.sign({ id: user._id, username: user.user.username }, secret.jwtSecret, { expiresIn: "10h" })
       ctx.result = {
         userInfo: {
           id: user._id,
@@ -28,16 +43,30 @@ module.exports = {
           nickname: user.nickname,
           last_login_time: user.last_login_time
         },
-        token: jwt.sign(
-          {
-            data: user._id,
-            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 3 // 3个小时
-          },
-          secret.jwtSecret
-        )
+        token: createToken(user)
       }
       // 更新用户表的最近一次的登陆时间
       UserService.updateById(user._id, { last_login_time: localTime(Date.now()) })
+    }
+    return next()
+  },
+  // 路由闯入编辑器页面进行token验证
+  'GET /api/confirmToken': async (ctx, next) => {
+    if (!ctx.headers.authorization) {
+      // 如果不指定状态码，默认为 500。
+      // 5xx 类错误 expose 默认为 false ，即不会将错误信息返回到 response
+      ctx.throw(401, 'UnauthorizedError')
+      // ctx.throw(401, 'UnauthorizedError', { code: 0, msg: '没有token', data: null })
+    } else {
+      const token = ctx.headers.authorization.split(' ')[1]
+      jwt.verify(token, secret.jwtSecret, function (err, decoded) {
+        if (err) {
+          console.log(err)
+          ctx.throw(401, 'UnauthorizedError')
+        } else {
+          ctx.result = decoded
+        }
+      })
     }
     return next()
   }
