a01b0df681418f357b7fe0b58cea609d8de6c087
xuyangyang2019 <15268296911@163.com>
2021-01-18 18:41:10 +0800

    分页查询文章

--- src/api/front.js
+++ src/api/front.js
@@ -29,4 +29,15 @@ function getTags(publish) {
   return fetch.get('/api/tags', { publish: publish })
 }
 
-export { getTags, getArticlesByTime, getHotArticles }
+/**
+ * 分页查询文章
+ * @param {Boolean} publish 是否发布
+ * @param {String} tag 标签
+ * @param {Number} pageNum 页码
+ * @param {Number} pageSize 每页数据量
+ */
+function getArticleList(publish, tag, pageNum, pageSize) {
+  return fetch.get('/api/getArticleList', { publish: publish, tag: tag, pageNum: pageNum, pageSize: pageSize })
+}
+
+export { getTags, getArticlesByTime, getHotArticles, getArticleList }
