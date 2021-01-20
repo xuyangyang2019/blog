341ec45a67728f36a1d890aa8ed495aff2cb6f71
xuyangyang2019 <15268296911@163.com>
2021-01-20 17:59:19 +0800

    游客登陆

--- server/controllers/VistorController.js
+++ server/controllers/VistorController.js
@@ -1,37 +1,42 @@
-const jwt = require('jsonwebtoken')
-const tokenConfig = require('../config').tokenConfig
-const UserService = require('../services').UserService
+// const jwt = require('jsonwebtoken')
+// const tokenConfig = require('../config').tokenConfig
+
+const VistorService = require('../services').VistorService
 const { InvalidQueryError } = require('../lib/error')
 
 module.exports = {
   'POST /api/vistorLogin': async (ctx, next) => {
-    const { username, password } = ctx.request.body
-    if (!username || !password) {
+    const { userName, password } = ctx.request.body
+    console.log('vistorLogin', ctx.request.body)
+    if (!userName || !password) {
       throw new InvalidQueryError()
     }
-    const user = await UserService.findOne({ username })
-    if (!user) {
+    const vistor = await VistorService.findOne({ userName: userName })
+    if (!vistor) {
       ctx.error = '用户不存在'
       ctx.code = 0
-    } else if (user.password !== password) {
+    } else if (vistor.password !== password) {
       ctx.error = '密码错误'
     } else {
-      ctx.result = {
-        userInfo: {
-          id: user._id,
-          username: user.username,
-          nickname: user.nickname,
-        },
-        token: jwt.sign({
-          data: user._id,
-          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3), //设置 token 过期时间: 3d
-        }, tokenConfig.secret)
-      }
+      ctx.result = vistor
+      // ctx.result = {
+      //   userInfo: {
+      //     id: vistor._id,
+      //     username: vistor.username,
+      //     nickname: vistor.nickname
+      //   },
+      //   token: jwt.sign(
+      //     {
+      //       data: user._id,
+      //       exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 3 // 设置 token 过期时间: 3d
+      //     },
+      //     tokenConfig.secret
+      //   )
+      // }
     }
-
     return next()
   },
-  'POST /api/register': async (ctx, next) => {
+  'POST /api/vistorRegister': async (ctx, next) => {
     const { username, password } = ctx.request.body
     if (!username || !password) {
       throw new InvalidQueryError()
@@ -44,15 +49,72 @@ module.exports = {
         userInfo: {
           id: user._id,
           username: user.username,
-          nickname: user.nickname,
+          nickname: user.nickname
         },
-        token: jwt.sign({
-          data: user._id,
-          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 3), //设置 token 过期时间: 3d
-        }, tokenConfig.secret)
+        token: jwt.sign(
+          {
+            data: user._id,
+            exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 3 // 设置 token 过期时间: 3d
+          },
+          tokenConfig.secret
+        )
       }
     }
 
     return next()
   }
+  // //github登录
+  // router.get("/api/getGithub",(req,res) => {
+  // 	db.vistor.find({githubID: req.query.id},(err,doc) => {
+  // 		if(err){
+  // 			res.status(500).end()
+  // 		}else{
+  // 			res.json(doc)
+  // 		}
+  // 	})
+  // })
+  // router.get("/login/git",(req,res) => {
+  // 	//请替换为自己的client_id
+  // 	let path = "https://github.com/login/oauth/authorize?client_id=YourID&scope=['user']"
+  // 	res.redirect(path)
+  // })
+  // router.get("/login_github",(req,res) => {
+  // 	//请替换为自己的client_id和client_secret
+  // 	let params = {
+  // 		client_id: "YourID",
+  // 		client_secret: "Your Secret",
+  // 		code: req.query.code,
+  // 		scope: ["user"],
+  // 		redirect_uri: "http://localhost:6180/login_github"
+  // 	}
+  // 	api.post("https://github.com/login/oauth/access_token",params)
+  // 	.then((fullData) =>{
+  // 		let arr1 = fullData.split("&"),
+  // 			arr2 = arr1[0].split("="),
+  // 			token = arr2[1]
+  // 		return token
+  // 	}).then((token) => {
+  // 		api.get("https://api.github.com/user",{access_token: token}).then((user_info) => {
+  // 			db.vistor.find({githubID: user_info.id},(err,doc) =>{
+  // 				if(err){
+  // 					res.status(500).end()
+  // 				}else{
+  // 					if(!doc.length){
+  // 						new db.vistor({
+  // 							name: user_info.login,
+  // 							imgUrl: user_info.avatar_url,
+  // 							githubID: user_info.id
+  // 						}).save()
+  // 					}
+  // 					res.cookie("githubId",user_info.id,{maxAge: 1000*60*60*24})
+  // 					res.status(200).end()
+  // 				}
+  // 			})
+  // 		}).catch((err) => {
+  // 			res.status(500).end()
+  // 		})
+  // 	}).catch((err) =>{
+  // 		res.status(500).end()
+  // 	})
+  // })
 }
