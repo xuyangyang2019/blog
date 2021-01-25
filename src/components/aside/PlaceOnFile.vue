81227aefb4344111d8545d33b4457709d87059ca
xuyangyang2019 <15268296911@163.com>
2021-01-25 18:26:15 +0800

    修改ui,以及数据

--- src/components/aside/PlaceOnFile.vue
+++ src/components/aside/PlaceOnFile.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="timeLine">
     <h1 class="timeLine-header">归档</h1>
-    <div class="timeLine-content">
-      <ul>
-        <li v-for="(item, index) in articles.time" :key="index">
-          <!-- <a href="jacascript: void(0)" @click="jumpTime(item.time)"> {{ item.time }}({{ item.num }})</a> -->
-          <a :href="jumpTime(item.time)" target="_blank">{{ item.time }}({{ item.num }})</a>
-        </li>
-      </ul>
-    </div>
+    <ul class="timeLine-content">
+      <li v-for="(item, index) in placeOnFile" :key="index" class="time-line" @click="jumpTime(item.time)">
+        {{ item.time }} ({{ item.num }})
+        <!-- <a href="jacascript: void(0)" @click="jumpTime(item.time)"> {{ item.time }}({{ item.num }})</a> -->
+        <!-- <a :href="jumpTime(item.time)" target="_blank">{{ item.time }}({{ item.num }})</a> -->
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -20,19 +19,19 @@ export default {
   name: 'PlaceOnFile',
   computed: {
     ...mapState({
-      articles: 'articles'
+      placeOnFile: 'placeOnFile'
     })
   },
   mounted() {
     getArticlesByTime(true).then((res) => {
       if (res.code === 200) {
-        this.SET_ARTICLES_TIME(res.data || [])
+        this.SET_PLACE_ON_FILE(res.data || [])
       }
     })
   },
   methods: {
     ...mapMutations({
-      SET_ARTICLES_TIME: 'SET_ARTICLES_TIME'
+      SET_PLACE_ON_FILE: 'SET_PLACE_ON_FILE'
     }),
     // 跳转到时间轴
     jumpTime(time) {
@@ -61,8 +60,8 @@ export default {
         }
       }
       date = year + '-' + month + '-' + '01' + 'to' + year + '-' + month + '-' + endDay
-      // this.$router.push({ name: "timeLine", params: { time: date } })
-      return '/timeline/' + date
+      // return '/placeOnFile/' + date
+      this.$router.push({ name: 'placeOnFile', params: { time: date } })
     }
   }
 }
@@ -71,29 +70,34 @@ export default {
 <style lang="scss" scoped>
 .timeLine {
   background: #faf7f7;
-}
-.timeLine-header {
-  text-align: center;
-  padding: 15px;
-  color: #eee;
-  background: #2e3033;
-}
-.timeLine-content {
-  padding: 5px 10px 35px;
-  li {
-    list-style: none;
-    border-bottom: 1px solid #ddd;
+  .timeLine-header {
+    text-align: center;
+    padding: 15px;
+    color: #eee;
+    background: #2e3033;
   }
-  a {
-    display: block;
-    justify-content: space-between;
-    width: 120px;
-    padding: 10px;
-    margin: 0 auto;
-    color: #16a085;
-  }
-  a:hover {
-    text-decoration: underline;
+  .timeLine-content {
+    padding: 5px 10px 35px;
+    li {
+      list-style: none;
+      cursor: pointer;
+      width: 120px;
+      padding: 10px;
+      margin: 0 auto;
+      color: #16a085;
+      border-bottom: 1px solid #ddd;
+    }
+    // a {
+    //   display: block;
+    //   justify-content: space-between;
+    //   width: 120px;
+    //   padding: 10px;
+    //   margin: 0 auto;
+    //   color: #16a085;
+    // }
+    // a:hover {
+    //   text-decoration: underline;
+    // }
   }
 }
 </style>
