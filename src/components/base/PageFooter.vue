88495fc7485a4ce30d63dea6596388ab7a2debfe
xuyangyang2019 <15268296911@163.com>
2021-01-27 14:32:16 +0800

    修改文件名

--- src/components/base/PageFooter.vue
+++ src/components/base/PageFooter.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="foot">
-    <div class="police">
+    <!-- <div class="police">
       <img src="/img/beian.png" />
       <p>鲁公网安备 37012502000331号</p>
-    </div>
+    </div> -->
     <div class="copyright">
       <div>
-        Copyright © 2020&nbsp
-        <a href="http://www.xyy.ink">xyy.ink &nbsp</a>
+        Copyright © 2020&nbsp;
+        <a href="http://www.xyy.life">xyy.life &nbsp;</a>
       </div>
       <div>
         Powered by Vue
         <span>|</span>
       </div>
       <div>
-        <a href="http://localhost:6180/login" target="_blank">&nbsp管理员</a>
+        <a href="http://localhost:6180/login" target="_blank">&nbsp;管理员</a>
       </div>
     </div>
   </div>
@@ -26,34 +26,38 @@
 .foot {
   color: #ccc;
   background: #2e3033;
+  /* height: 50px; */
   /* display: flex; */
-  a {
+  /* border: solid red 1px; */
+  padding: 10px 0;
+
+  /* a {
     color: #ccc;
   }
   a:hover {
     color: #ffffff;
     text-decoration: underline;
-  }
-  .police {
+  } */
+  /* .police {
     display: flex;
     justify-content: center;
     padding: 20px;
-  }
+  } */
   .copyright {
     display: flex;
-    justify-content: center;
     flex-wrap: wrap;
-    padding-bottom: 25px;
+    justify-content: center;
     font-family: HeiTi;
+    /* border: solid yellow 1px; */
   }
 }
 
 @media screen and(max-width: 767px) {
   .foot {
     font-size: 14px;
-    .police {
+    /* .police {
       padding: 15px;
-    }
+    } */
     .copyright span {
       display: none;
     }
