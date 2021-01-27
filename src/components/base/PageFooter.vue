a671ecdc259ecbef000ed516300feeacb887dd28
xuyangyang2019 <15268296911@163.com>
2021-01-27 15:09:48 +0800

    修改ui

--- src/components/base/PageFooter.vue
+++ src/components/base/PageFooter.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="foot">
-    <!-- <div class="police">
-      <img src="/img/beian.png" />
-      <p>鲁公网安备 37012502000331号</p>
-    </div> -->
+  <footer class="page-footer">
     <div class="copyright">
       <div>
         Copyright © 2020&nbsp;
@@ -17,47 +13,29 @@
         <a href="http://localhost:6180/login" target="_blank">&nbsp;管理员</a>
       </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script></script>
 
 <style lang = "scss">
-.foot {
+.page-footer {
   color: #ccc;
   background: #2e3033;
-  /* height: 50px; */
-  /* display: flex; */
-  /* border: solid red 1px; */
   padding: 10px 0;
 
-  /* a {
-    color: #ccc;
-  }
-  a:hover {
-    color: #ffffff;
-    text-decoration: underline;
-  } */
-  /* .police {
-    display: flex;
-    justify-content: center;
-    padding: 20px;
-  } */
   .copyright {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    align-items: center;
     font-family: HeiTi;
-    /* border: solid yellow 1px; */
   }
 }
 
 @media screen and(max-width: 767px) {
-  .foot {
+  .page-footer {
     font-size: 14px;
-    /* .police {
-      padding: 15px;
-    } */
     .copyright span {
       display: none;
     }
