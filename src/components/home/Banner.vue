20f3304a2ac29c1fa9b8e740bf2b91bceb5dce6c
xuyangyang2019 <15268296911@163.com>
2021-02-01 17:04:57 +0800

    修改轮播图的实现方式，适配移动端的滑动

--- src/components/home/Banner.vue
+++ src/components/home/Banner.vue
@@ -12,7 +12,9 @@
         @touchstart.stop="touchStart($event)"
         @touchend.stop="touchEnd($event)"
       >
-        <img ref="img" class="banner-img" :data-src="item.url" alt="" src="/img/pic-loading.gif" />
+        <!-- <img ref="img" class="banner-img" :data-src="item.url" alt="" src="/img/pic-loading.gif" /> -->
+        <img v-show="currentIndex === index" class="banner-img" :src="item.url" />
+
         <div class="banner-words">
           <div class="wellknown">
             <p>{{ item.word }}</p>
@@ -44,82 +46,78 @@ export default {
   data() {
     return {
       currentIndex: 0, // 当前的banner
-      startPos: { x: '', y: '', date: '' },
-      move: { x: '', y: '' },
       bannerData: [
         {
-          url: '/img/banner/one.jpeg',
+          url: 'http://192.168.0.111:8098/img/banner/one.jpeg',
           word: 'Success is not final, failure is not fatal. It is the courage to continue that counts.',
           person: 'Winston Churchill'
         },
         {
-          url: '/img/banner/two.jpeg',
+          url: 'http://192.168.0.111:8098/img/banner/two.jpeg',
           word:
             '生命中最伟大的光辉不在于永不坠落，而是坠落后总能再度升起。我欣赏这种有弹性的生命状态，快乐地经历风雨，笑对人生。',
           person: '曼德拉'
         },
         {
-          url: '/img/banner/three.jpeg',
+          url: 'http://192.168.0.111:8098/img/banner/three.jpeg',
           word: '时间是一只藏在黑暗中的温柔的手，在你一出神一恍惚之间，物走星移。',
           person: '龙应台'
         },
         {
-          url: '/img/banner/four.jpeg',
+          url: 'http://192.168.0.111:8098/img/banner/four.jpeg',
           word: '一个人可以被毁灭，但不能被打败。',
           person: '海明威'
         },
         {
-          url: '/img/banner/five.jpeg',
+          url: 'http://192.168.0.111:8098/img/banner/five.jpeg',
           word:
             '我要纵身跳入时代的奔走，我要纵身跳入时代的年轮：苦痛，欢乐，失败，成功，我都不问，男儿的事业原本要昼夜不停。',
           person: '歌德'
         }
-      ] // banner信息
+      ], // banner信息
+      startPos: { x: '', y: '', date: '' }, // 移动端点击的初始位置和时间
+      move: { x: '', y: '' } // 移动端滑动的位置
     }
   },
   mounted() {
-    this.lazyLoad()
+    this.play()
+  },
+  beforeDestroy() {
+    // 清除定时器
+    clearInterval(this.timer)
   },
   methods: {
-    // 实现图片懒加载
-    lazyLoad() {
-      this.$refs.img.forEach((item, index) => {
-        if (index === this.currentIndex) {
-          // 清除定时器，防止图片还没加载完成就轮播到下一张
-          clearInterval(this.timer)
-          const img = new Image()
-          img.src = item.dataset.src
-          img.onload = () => {
-            item.src = img.src
-            this.slider()
-          }
-        }
-      })
+    // 开始轮播
+    play() {
+      if (this.timer) {
+        clearInterval(this.timer)
+      }
+      this.autoPlay()
     },
-    // 滑动
-    slider() {
+    // 轮播逻辑
+    autoPlay() {
       this.timer = setInterval(() => {
-        if (this.currentIndex < this.bannerData.length - 1) {
-          this.currentIndex++
-          this.lazyLoad()
-        } else {
+        this.$data.currentIndex++
+        if (this.currentIndex >= this.bannerData.length) {
           this.currentIndex = 0
-          this.lazyLoad()
+        }
+        if (this.currentIndex < 0) {
+          this.currentIndex = this.bannerData.length
         }
       }, 5000)
     },
-    // 选择图片
-    chosePic(index) {
-      this.currentIndex = index
-      this.lazyLoad()
-    },
-    // 开始滑动
-    startSlider() {
-      this.lazyLoad()
-    },
-    // 停止滑动
+    // 鼠标悬停 停止滑动
     stopSlider() {
       clearInterval(this.timer)
+    },
+    // 选择图片
+    chosePic(index) {
+      // 显示当前点击的图片
+      this.currentIndex = index
+    },
+    // 鼠标离开 开始滑动
+    startSlider() {
+      this.play()
     },
     // 移动端也会触发mouseover事件（奇怪--），所以做一下判断，否则手动切换图片会消除定时器
     stopSlider_cpt() {
@@ -131,39 +129,93 @@ export default {
     // 移动端滑动
     touchStart(event) {
       clearInterval(this.timer)
-      const touch = event.targetTouches[0] // touches数组对象获得屏幕上所有的touch，取第一个touch
-      this.startPos = { x: touch.pageX, y: touch.pageY, date: +new Date() } // 取第一个touch的坐标值
+      // touches数组对象获得屏幕上所有的touch，取第一个touch
+      const touch = event.targetTouches[0]
+      // console.log('touchStart', event.targetTouches)
+      // 取第一个touch的坐标值
+      this.startPos = { x: touch.pageX, y: touch.pageY, date: new Date().getTime() }
     },
-    touchMove: function (event) {
+    touchMove(event) {
       // 防止滚屏
       event.preventDefault()
       if (event.targetTouches.length > 1 || (event.scale && event.scale !== 1)) return
+      // console.log('touchMove', event.targetTouches)
       const touch = event.targetTouches[0]
+      // console.log('touchMove', touch)
       this.move.x = touch.pageX - this.startPos.x
       this.move.y = touch.pageY - this.startPos.y
     },
     touchEnd() {
-      // const ted = event.targetTouches[0] // touches数组对象获得屏幕上所有的touch，取第一个touch
-      const moveDate = +new Date() - this.startPos.date
-      if (Math.abs(this.move.x) > Math.abs(this.move.y) && moveDate < 1000 && this.move.x < -150) {
-        if (this.currentIndex < this.bannerData.length - 1) {
-          this.currentIndex++
-          this.move = { x: 0, y: 0, date: '' }
+      console.log(this.move)
+      const changeX = this.move.x
+      const changeY = this.move.y
+      const changeDate = new Date().getTime() - this.startPos.date
+      // console.log(changeDate)
+      // 长按事件
+      if (changeDate > 1000) return
+      // 也可以加上滑动长度限制
+      if (Math.abs(changeX) > 150) return
+      // 滑动和点击事件处理
+      if (Math.abs(changeX) > Math.abs(changeY)) {
+        // 左右事件
+        if (changeX > 0) {
+          // console.log('右滑')
+          if (this.currentIndex === 0) {
+            this.currentIndex = this.bannerData.length - 1
+          } else {
+            this.currentIndex--
+          }
         } else {
-          this.currentIndex = 0
-          this.move = { x: 0, y: 0, date: '' }
+          // console.log('左滑')
+          if (this.currentIndex >= this.bannerData.length - 1) {
+            this.currentIndex = 0
+          } else {
+            this.currentIndex++
+          }
         }
-      }
-      if (Math.abs(this.move.x) > Math.abs(this.move.y) && moveDate < 1000 && this.move.x > 150) {
-        if (this.currentIndex > 0) {
-          this.currentIndex--
-          this.move = { x: 0, y: 0, date: '' }
+      } else if (Math.abs(changeY) > Math.abs(changeX)) {
+        // 上下事件
+        if (changeY > 0) {
+          // console.log('下滑')
         } else {
-          this.currentIndex = this.bannerData.length - 1
-          this.move = { x: 0, y: 0, date: '' }
+          // console.log('上滑')
         }
+      } else {
+        // console.log('点击')
       }
-      this.lazyLoad()
+      // 重置move
+      this.move = { x: 0, y: 0, date: '' }
+      // 开始轮播
+      this.play()
+    },
+    // ============================= 另外的方法实现轮播 ==================================
+    // 实现图片懒加载
+    lazyLoad() {
+      // this.$refs.img.forEach((item, index) => {
+      //   if (index === this.currentIndex) {
+      //     // 清除定时器，防止图片还没加载完成就轮播到下一张
+      //     clearInterval(this.timer)
+      //     const img = new Image()
+      //     img.src = item.dataset.src
+      //     img.onload = () => {
+      //       item.src = img.src
+      //       this.slider()
+      //     }
+      //   }
+      // })
+    },
+    // 滑动
+    slider() {
+      console.log('滑动')
+      // this.timer = setInterval(() => {
+      //   if (this.currentIndex < this.bannerData.length - 1) {
+      //     this.currentIndex++
+      //     this.lazyLoad()
+      //   } else {
+      //     this.currentIndex = 0
+      //     this.lazyLoad()
+      //   }
+      // }, 5000)
     }
   }
 }
