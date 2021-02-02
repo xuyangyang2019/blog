efa21b941c2f1fbfe3ee6a9e0d1d9f08b4f9d8a4
xuyangyang2019 <15268296911@163.com>
2021-02-02 19:18:50 +0800

    修改日志事件的显示格式

--- pm2.config.js
+++ pm2.config.js
@@ -8,7 +8,7 @@ module.exports = {
       error: './log/error.log',
       log: './log/combined.outerr.log',
       merge_logs: true, // 集群的所有实例的日志文件合并
-      log_date_format: 'DD-MM-YYYY',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
       instances: 1, // 进程数 1、数字 2、'max'根据cpu内核数
       max_memory_restart: '1G', // 当内存超过1024M时自动重启
       watching: true,
