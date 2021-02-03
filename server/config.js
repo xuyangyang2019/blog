fba205246804d7b0e8b617859a20c3f5ca7a2936
xuyangyang2019 <15268296911@163.com>
2021-02-03 12:23:57 +0800

    添加server的配置文件参数

--- server/config.js
+++ server/config.js
@@ -1,14 +1,10 @@
 const path = require('path')
+const currentIP = require('ip').address()
 
 module.exports = {
-  // port: '10081',
-  tokenConfig: {
-    secret: 'havana',
-    expired: 60 * 60 * 24 // 1h
-  },
-  publicDir: path.resolve(__dirname, './public'),
-  // viewsDir: path.resolve(__dirname, './views'),
-  logPath: path.resolve(__dirname, './logs/koa-server'),
+  devHost: 'localhost', // 开发时的host
+  prodHost: currentIP, // 线上的host
+  serverPort: 8098, // 服务器端口
   mongoDB: {
     host: '127.0.0.1',
     port: 27017,
@@ -16,5 +12,13 @@ module.exports = {
     pwd: '123456',
     db: 'blog',
     authSource: 'admin'
-  }
+  }, // mongodb配置文件
+  // =====================================
+  tokenConfig: {
+    secret: 'havana',
+    expired: 60 * 60 * 24 // 1h
+  },
+  publicDir: path.resolve(__dirname, './public'),
+  // viewsDir: path.resolve(__dirname, './views'),
+  logPath: path.resolve(__dirname, './logs/koa-server')
 }
