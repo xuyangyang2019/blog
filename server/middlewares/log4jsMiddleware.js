7e7b7a603ba5a0d55323caa0cfb759e12e1cdfc1
xuyangyang2019 <15268296911@163.com>
2021-02-07 17:51:05 +0800

    log4js中间件

--- server/middlewares/log4jsMiddleware.js
+++ server/middlewares/log4jsMiddleware.js
@@ -1,34 +1,63 @@
-const log4js = require('log4js')
-const logsConfig = require('../config/log4jsConfig.js')
+const { logger } = require('../utils/log4jsUtil')
 
-// 加载配置文件
-log4js.configure(logsConfig)
-
-// 调用预先定义的日志名称
-const logger = log4js.getLogger('default')
-const httpLogger = log4js.getLogger('httpLogger')
-const errorLogger = log4js.getLogger('errorLogger')
-const handleLogger = log4js.getLogger('handleLogger')
-
-const loggerMiddleware = async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
+/**
+ * @param {*} ctx ctx
+ * @method 获取客户端ip地址
+ */
+function getClientIp(ctx) {
+  // return (
+  //   req.headers['x-forwarded-for'] ||
+  //   req.connection.remoteAddress ||
+  //   req.socket.remoteAddress ||
+  //   req.connection.socket.remoteAddress
+  // )
   const remoteAddress =
     ctx.headers['x-forwarded-for'] ||
     ctx.ip ||
     ctx.ips ||
     (ctx.socket && (ctx.socket.remoteAddress || (ctx.socket.socket && ctx.socket.socket.remoteAddress)))
-  const logText = `${ctx.method} ${ctx.status} ${ctx.url} 请求参数： ${JSON.stringify(
-    ctx.request.body
-  )}  响应参数： ${JSON.stringify(ctx.body)} - ${remoteAddress} - ${ms}ms`
-  logger.info(logText)
+  return remoteAddress
+}
+
+function isMobile(userAgent) {
+  // 判断是移动端还是pc端
+  return /Mobile/.test(userAgent) ? 'Mobile' : 'PC'
+}
+
+const log4jsMiddleware = async (ctx, next) => {
+  // console.log(ctx)
+  const start = new Date() // 响应开始时间
+  await next()
+  const pesponsTime = new Date() - start // 响应间隔时间
+  // ${JSON.stringify(ctx.body)}
+
+  // const ip = getClientIp(ctx) // ip
+  // const userAgent = isMobile(ctx.request.headers['user-agent']) // 电脑还是手机
+  // const method = ctx.request.method // 请求方法
+  // const path = ctx.request.path // 请求路径
+  // const referer = ctx.request.headers['referer'] // 推荐人
+  // const status = ctx.status // 结果
+  // const logTextStr = `${userAgent} ${ip} ${method} ${path} 请求参数： ${JSON.stringify(
+  //   ctx.request.body
+  // )} - ${status} - ${pesponsTime}ms`
+
+  // 返回客户端信息交给logger打印
+  const logTextJson = {
+    ip: getClientIp(ctx),
+    userAgent: isMobile(ctx.request.headers['user-agent']),
+    method: ctx.request.method,
+    path: ctx.request.path,
+    url: ctx.url,
+    request: ctx.request,
+    body: ctx.body,
+    referer: ctx.request.headers['referer'],
+    status: ctx.status,
+    responseTime: pesponsTime + 'ms'
+  }
+  // 控制台输出日志
+  logger.info(logTextJson)
 }
 
 module.exports = {
-  logger,
-  httpLogger,
-  errorLogger,
-  handleLogger,
-  loggerMiddleware
+  log4jsMiddleware
 }
