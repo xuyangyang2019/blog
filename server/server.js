a0d6b83a11fd11c173c84b7d2102821239e6b547
xuyangyang2019 <15268296911@163.com>
2021-02-19 14:50:21 +0800

    服务端配置跨域参数

--- server/server.js
+++ server/server.js
@@ -83,7 +83,22 @@ if (!isProd) {
 // backendApp.use(rest.restify())
 
 // Cors
-backendApp.use(cors())
+backendApp.use(
+  cors({
+    origin: function (ctx) {
+      // 设置允许来自指定域名请求
+      if (ctx.url === '/test') {
+        return '*' // 允许来自所有域名请求
+      }
+      return uri // 只允许http://localhost:8080这个域名的请求
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'], // 设置获取其他自定义字段
+    maxAge: 5, // 指定本次预检请求的有效期，单位为秒。
+    credentials: true, // 是否允许发送Cookie
+    allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // 设置所允许的HTTP请求方法
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'] // 设置服务器支持的所有头信息字段
+  })
+)
 
 // // View
 // backendApp.use(views(config.viewsDir))
