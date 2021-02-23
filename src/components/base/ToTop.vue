45831fcb3c798915a030953d1ec95b03a675da78
xuyangyang2019 <15268296911@163.com>
2021-02-23 18:35:11 +0800

    修改ui,添加动画效果

--- src/components/base/ToTop.vue
+++ src/components/base/ToTop.vue
@@ -1,22 +1,35 @@
 
 <template>
-  <div ref="btn" class="back-to-top" @click="totop">返回</div>
+  <transition name="fade">
+    <div v-show="showRocket" ref="btn" class="back-to-top" @click="goTop">
+      <i class="fa fa-rocket fa-2x" aria-hidden="true"></i>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'ToTop',
   props: ['domName'],
+  data() {
+    return {
+      showRocket: false
+    }
+  },
   mounted() {
+    // 监听滚动事件
     this.scroll()
   },
   methods: {
-    totop() {
+    // 回到top
+    goTop() {
+      // 锚点
       const body = window.document.querySelector(this.domName)
       let stepNum = 10
       let scrollTop = body
         ? body.scrollTop
         : document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
+
       const timeId = setInterval(() => {
         stepNum += stepNum
         if (scrollTop <= 0) clearInterval(timeId)
@@ -30,16 +43,24 @@ export default {
         }
       }, 80)
     },
+    // 监听滚动控制图标展示或隐藏
     scroll() {
       const body = window.document.querySelector(this.domName || 'body')
       body.onscroll = () => {
         const scrollTop =
           body.scrollTop || document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
-        const innerHeight = window.innerHeight - 500
-        const opacityNum = 1 - (innerHeight - scrollTop) / innerHeight
-        const resNum = opacityNum > 1 ? 1 : opacityNum
-        this.$refs['btn'].style.opacity = scrollTop <= 0 ? 0 : resNum
-        this.$refs['btn'].style.display = scrollTop > innerHeight ? 'block' : 'none'
+        // 通过修改样式控制显示/隐藏火箭
+        // const innerHeight = window.innerHeight - 500
+        // const opacityNum = 1 - (innerHeight - scrollTop) / innerHeight
+        // const resNum = opacityNum > 1 ? 1 : opacityNum
+        // this.$refs['btn'].style.opacity = scrollTop <= 0 ? 0 : resNum
+        // this.$refs['btn'].style.display = scrollTop > innerHeight ? 'block' : 'none'
+        // 通过js控制隐现
+        if (scrollTop > 500) {
+          this.showRocket = true
+        } else {
+          this.showRocket = false
+        }
       }
     }
   }
@@ -47,16 +68,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: all ease 0.5s;
+}
+
 .back-to-top {
-  position: absolute;
-  right: 0.2rem;
-  bottom: 1rem;
-  width: 1rem;
-  height: 1rem;
-//   opacity: 0;
+  position: fixed;
+  right: 20px;
+  bottom: 50px;
+  color: #909399;
+  .fa-rocket {
+    transform: rotate(315deg);
+  }
+  &:hover {
+    cursor: pointer;
+    color: #c0c4cc;
+  }
+  //   opacity: 0;
   //   display: none;
-  //   background: url('../assets/img/ToTop.png') center no-repeat;
-  //   background-size: cover;
-  //   z-index: 9;
 }
 </style>
