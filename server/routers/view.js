5c7ddf94991737f643879c2c72f0e7273681fc22
xuyangyang2019 <15268296911@163.com>
2021-06-29 18:45:03 +0800

    管理端页面

--- server/routers/view.js
+++ server/routers/view.js
@@ -55,9 +55,10 @@ router.get(
 )
 
 // 后台页面
-// router.get(['/admin', '/admin/*', '/login'], async (ctx) => {
-//   const html = fs.readFileSync(path.join(__dirname, '../../dist-admin/index.html'), 'utf-8')
-//   ctx.body = html
-// })
+router.get(['/admin', '/admin/*', '/login'], async (ctx) => {
+  // const html = fs.readFileSync(path.join(__dirname, '../../dist-admin/index.html'), 'utf-8')
+  const html = 'admin'
+  ctx.body = html
+})
 
 module.exports = router
