a57b6899059b3b3bb72633aa781dd983f1b45146
xuyangyang2019 <15268296911@163.com>
2021-07-05 12:22:04 +0800

    添加rest api

--- server/routers/index.js
+++ server/routers/index.js
@@ -4,14 +4,17 @@
 
 const router = require('koa-router')()
 
-// const api = require('./api')
-const home = require('./home')
-const admin = require('./admin')
+// const api = require('./api')() // rest api
+const api = require('../api/index') // rest api
+const home = require('./home') // 客户端页面路由
+const admin = require('./admin') // 管理端页面路由
 
 // router.use('/api', api.routes(), api.allowedMethods())
 // router.use('/', home.routes(), home.allowedMethods())
 // router.use('/admin', admin.routes(), admin.allowedMethods())
 
+router.use(api.routes(), api.allowedMethods())
 router.use(home.routes(), home.allowedMethods())
 router.use(admin.routes(), admin.allowedMethods())
+
 module.exports = router
