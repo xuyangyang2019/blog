d5e8f1841c930195b87eb7caf028cbc147b43360
xuyangyang2019 <15268296911@163.com>
2021-07-05 11:48:06 +0800

    自动注册当前文件夹里的路由文件

---
+++ server/api/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const router = require('koa-router')()
+
+// 扫描当前文件夹下所有的js文件,除了index.js
+const files = fs.readdirSync(__dirname).filter((file) => file.endsWith('.js') && file !== 'index.js')
+
+files.forEach((file) => {
+  // eslint-disable-next-line global-require
+  const mapping = require(`./${file}`)
+  for (const url in mapping) {
+    if (url.startsWith('GET ')) {
+      const path = url.substring(4)
+      router.get(path, mapping[url])
+    } else if (url.startsWith('POST ')) {
+      const path = url.substring(5)
+      router.post(path, mapping[url])
+    } else if (url.startsWith('PUT ')) {
+      const path = url.substring(4)
+      router.put(path, mapping[url])
+    } else if (url.startsWith('PATCH ')) {
+      const path = url.substring(6)
+      router.patch(path, mapping[url])
+    } else if (url.startsWith('DELETE ')) {
+      const path = url.substring(7)
+      router.del(path, mapping[url])
+    } else {
+      console.log(`invalid URL: ${url}`)
+    }
+  }
+})
+
+module.exports = router
