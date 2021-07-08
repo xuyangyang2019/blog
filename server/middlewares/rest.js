0b3448de409240865517d9b70f2dc6e51087ac59
xuyangyang2019 <15268296911@163.com>
2021-07-08 17:51:49 +0800

    给ctx添加一个rest()方法

--- server/middlewares/rest.js
+++ server/middlewares/rest.js
@@ -1,36 +1,19 @@
-module.exports = {
-  APIError: function (code, message) {
-    this.code = code || 'internal:unknown_error'
-    this.message = message || ''
-  },
-  restify: (pathPrefix) => {
-    // REST API前缀，默认为/api/:
-    pathPrefix = pathPrefix || '/api/'
-    return async (ctx, next) => {
-      // 是否是REST API前缀?
-      if (ctx.request.path.startsWith(pathPrefix)) {
-        // 绑定rest()方法:
-        ctx.rest = (data) => {
-          ctx.response.type = 'application/json'
-          ctx.response.body = {
-            code: ctx.code || 200,
-            msg: ctx.msg || 'success',
-            data: data
-          }
+module.exports = function (pathPrefix) {
+  // REST API前缀，默认为/api/:
+  pathPrefix = pathPrefix || '/api/'
+  return async (ctx, next) => {
+    // 是否是REST API前缀?
+    if (ctx.request.path.startsWith(pathPrefix)) {
+      // 绑定rest()方法:
+      ctx.rest = (data) => {
+        ctx.response.type = 'application/json'
+        ctx.response.body = {
+          code: ctx.code || 200,
+          message: ctx.message || 'success',
+          data: data
         }
-        try {
-          await next()
-        } catch (e) {
-          ctx.response.status = 400
-          ctx.response.type = 'application/json'
-          ctx.response.body = {
-            code: e.code || 'internal:unknown_error',
-            message: e.message || ''
-          }
-        }
-      } else {
-        await next()
       }
     }
+    await next()
   }
 }
