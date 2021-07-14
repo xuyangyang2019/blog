8ed052a4eaf23b0e37ac3d9d66d83fe4dc41e26d
xuyangyang2019 <15268296911@163.com>
2021-07-14 17:46:08 +0800

    修改鉴权逻辑

--- server/middlewares/confirmToken.js
+++ server/middlewares/confirmToken.js
@@ -1,31 +1,17 @@
-const jwtSecret = require('../config').jwtSecret
 const jwt = require('jsonwebtoken')
+const jwtSecret = require('../config').jwtSecret
 
-const confirmToken = (ctx, next) => {
-  // koa-jwt
-  // try {
-  //   if (typeof ctx.request.headers.authorization === 'string') {
-  //     const token = ctx.request.headers.authorization.slice(7)
-  //     ctx.jwtData = jwt.verify(token, jwtSecret)
-  //   } else {
-  //     throw { code: 401, message: 'no authorization' }
-  //   }
-  // } catch (err) {
-  //   throw { code: 401, message: err.message }
-  // }
-  if (!ctx.headers.authorization) {
-    ctx.body = { code: 401 }
-  } else {
-    // const token = ctx.headers.authorization
+const confirmToken = async (ctx, next) => {
+  if (ctx.request.headers.authorization) {
     const token = ctx.request.headers.authorization.slice(7)
-    jwt.verify(token, jwtSecret, (err) => {
-      if (err) {
-        throw { code: 401, message: 'no authorization' }
-      } else {
-        console.log('token验证通过')
-        next()
-      }
-    })
+    try {
+      jwt.verify(token, jwtSecret)
+      await next()
+    } catch (error) {
+      throw { code: 401, message: err.message }
+    }
+  } else {
+    throw { code: 401, message: 'no authorization' }
   }
 }
 
