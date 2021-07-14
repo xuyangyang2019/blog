7b818adc17f21927eb5b23f443c72b77eff30ade
xuyangyang2019 <15268296911@163.com>
2021-07-14 11:05:37 +0800

    修改鉴权逻辑

--- server/api/privite/index.js
+++ server/api/privite/index.js
@@ -1,13 +1,6 @@
 const fs = require('fs')
 const router = require('koa-router')()
-
-// const { logger } = require('../middlewares/logger')
-
-const config = require('../secret')
-const koaJwt = require('koa-jwt')({ secret: config.jwtSecret })
-
-// 校验接口，进行登录验证后才能访问
-router.use(koaJwt)
+const confirmToken = require('../../middlewares/confirmToken')
 
 // 扫描当前文件夹下所有的js文件,除了index.js
 const files = fs.readdirSync(__dirname).filter((file) => file.endsWith('.js') && file !== 'index.js')
@@ -18,19 +11,19 @@ files.forEach((file) => {
   for (const url in mapping) {
     if (url.startsWith('GET ')) {
       const path = url.substring(4)
-      router.get(path, mapping[url])
+      router.get(path, confirmToken, mapping[url])
     } else if (url.startsWith('POST ')) {
       const path = url.substring(5)
-      router.post(path, mapping[url])
+      router.post(path, confirmToken, mapping[url])
     } else if (url.startsWith('PUT ')) {
       const path = url.substring(4)
-      router.put(path, mapping[url])
+      router.put(path, confirmToken, mapping[url])
     } else if (url.startsWith('PATCH ')) {
       const path = url.substring(6)
-      router.patch(path, mapping[url])
+      router.patch(path, confirmToken, mapping[url])
     } else if (url.startsWith('DELETE ')) {
       const path = url.substring(7)
-      router.del(path, mapping[url])
+      router.del(path, confirmToken, mapping[url])
     } else {
       console.log(`invalid URL: ${url}`)
     }
