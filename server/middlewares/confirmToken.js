1e227b594c5141697f14bfd03f957aa25b1da66a
xuyangyang2019 <15268296911@163.com>
2021-07-16 14:32:53 +0800

    优化鉴权代码

--- server/middlewares/confirmToken.js
+++ server/middlewares/confirmToken.js
@@ -2,13 +2,9 @@ const jwt = require('jsonwebtoken')
 const jwtSecret = require('../config').jwtSecret
 
 const confirmToken = async (ctx, next) => {
-  if (ctx.request.headers.authorization) {
-    const token = ctx.request.headers.authorization.slice(7)
-    jwt.verify(token, jwtSecret)
-    await next()
-  } else {
-    ctx.throw(401, 'NoToken', { code: 0, data: null })
-  }
+  const token = ctx.request.headers.authorization ? ctx.request.headers.authorization.slice(7) : ''
+  jwt.verify(token, jwtSecret)
+  await next()
 }
 
 module.exports = confirmToken
