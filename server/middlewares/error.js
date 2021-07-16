f3df72e20b19412751ad8b8472ef5211d84939d4
xuyangyang2019 <15268296911@163.com>
2021-07-16 15:01:44 +0800

    鉴权错误处理和其他错误处理

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -4,33 +4,29 @@ const errorHandler = async (ctx, next) => {
   try {
     await next()
   } catch (err) {
-    console.log('errorHandler', err)
-    ctx.response.status = err.statusCode || err.status || 500
-    ctx.response.body = {
-      message: err.message
+    if (err.name === 'TokenExpiredError') {
+      // token超时
+      ctx.status = 403
+      ctx.body = err.message
+    } else if (err.name === 'JsonWebTokenError') {
+      // token错误
+      ctx.status = 401
+      ctx.body = err.message
+    } else {
+      console.log('errorHandler', err.name)
+      console.log('errorHandler', err.message)
+      // console.log('errorHandler', err.stack)
+      // 保证返回状态是 200, 这样前端不会抛出异常
+      ctx.status = 200
+      ctx.body = {
+        code: err.code || -1,
+        data: null,
+        message: err.message ? err.message.trim() : err
+      }
     }
-    // 手动释放error事件 可以被onerror捕捉
-    // ctx.app.emit('error', err, ctx)
+    // 手动释放error事件
+    ctx.app.emit('error', err, ctx)
   }
-  // return next().catch((err) => {
-  //   if (err.name === 'UnauthorizedError') {
-  //     // 身份验证失败返回 401
-  //     ctx.status = 401
-  //     ctx.body = err.originalError || 'Authentication Error'
-  //   } else {
-  //     if (err.code === null) {
-  //       logger.error(err.stack)
-  //     }
-  //     // 保证返回状态是 200, 这样前端不会抛出异常
-  //     ctx.status = 200
-  //     ctx.body = {
-  //       code: err.code || -1,
-  //       data: null,
-  //       msg: err.message ? err.message.trim() : err
-  //     }
-  //   }
-  //   return Promise.resolve()
-  // })
 }
 
 module.exports = errorHandler
