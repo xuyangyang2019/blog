9b55148bad96a3c1ea7aec39b7fe9a281e239699
xuyangyang2019 <15268296911@163.com>
2021-07-20 16:39:13 +0800

    数据库错误打出并保存到文件

--- server/lib/mongoDB.js
+++ server/lib/mongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 // 服务端的配置文件
 const config = require('../config').mongoDB
 // 日志
-const { logInfo } = require('../utils/log4js')
+const { logInfo, logError } = require('../utils/log4js')
 
 // const url = `mongodb://${config.user}:${config.pwd}@${config.host}:${config.port}/${config.db}?authSource=${config.authSource}`
 const url = `mongodb://${config.user}:${config.pwd}@${config.host}:${config.port}/${config.db}`
@@ -11,9 +11,9 @@ const mongo = mongoose.createConnection(url, { useNewUrlParser: true, useUnified
 
 mongo.on('connected', () => logInfo(`Connected to database: ${url}`))
 
-mongo.on('error', () => {
-  console.log(`Failed to connect to database: ${url}`)
-  // logger.error(`Failed to connect to database: ${url}`)
+mongo.on('error', (err) => {
+  logInfo(err)
+  logError(null, `Failed to connect to database: ${url}:${err}`)
 })
 
 mongo.on('disconnected', () => logInfo(`Closed connection to database: ${url}`))
