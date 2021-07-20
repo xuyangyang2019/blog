2d9aadceed970366662361a6895d809b05c0e693
xuyangyang2019 <15268296911@163.com>
2021-07-20 11:51:59 +0800

    修改log4jsMiddleware.js为logger,js

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -1,44 +1,47 @@
-const fs = require('fs')
-const path = require('path')
-const log4js = require('log4js')
-const config = require('../config')
+const { logInfo } = require('../utils/log4jsUtil')
 
-const logsDir = path.parse(config.logPath).dir
-if (!fs.existsSync(logsDir)) {
-  fs.mkdirSync(logsDir)
-}
-
-log4js.configure({
-  appenders: {
-    console: { type: 'console' },
-    dateFile: { type: 'dateFile', filename: config.logPath, pattern: 'yyyy-MM-dd.log', alwaysIncludePattern: true }
-  },
-  categories: {
-    default: {
-      appenders: ['console', 'dateFile'],
-      level: 'info'
-    }
-  }
-})
-
-const logger = log4js.getLogger('[Default]')
-
-const loggerMiddleware = async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
+/**
+ * 获取客户端ip地址
+ * @param {*} ctx
+ */
+function getClientIp(ctx) {
   const remoteAddress =
     ctx.headers['x-forwarded-for'] ||
     ctx.ip ||
     ctx.ips ||
     (ctx.socket && (ctx.socket.remoteAddress || (ctx.socket.socket && ctx.socket.socket.remoteAddress)))
-  const logText = `${ctx.method} ${ctx.status} ${ctx.url} 请求参数： ${JSON.stringify(
-    ctx.request.body
-  )}  响应参数： ${JSON.stringify(ctx.body)} - ${remoteAddress} - ${ms}ms`
-  logger.info(logText)
+  return remoteAddress
 }
 
-module.exports = {
-  logger,
-  loggerMiddleware
+/**
+ * 是手机还是pc
+ * @param {*} userAgent
+ */
+function isMobile(userAgent) {
+  // 判断是移动端还是pc端
+  return /Mobile/.test(userAgent) ? 'Mobile' : 'PC'
 }
+
+const log4jsMiddleware = async (ctx, next) => {
+  const start = new Date() // 响应开始时间
+  await next()
+  const execTime = new Date() - start // 响应间隔时间
+
+  // 返回客户端信息交给logger打印
+  const logTextJson = {
+    ip: getClientIp(ctx), // ip
+    userAgent: isMobile(ctx.request.headers['user-agent']), // 电脑还是手机
+    method: ctx.request.method, // 请求方法
+    path: ctx.request.path, // 请求路径
+    // url: ctx.url,
+    // request: ctx.request,
+    // body: ctx.body,
+    referer: ctx.request.headers['referer'], // 推荐人
+    status: ctx.status,
+    responseTime: execTime + 'ms'
+  }
+  // 控制台输出日志
+  logInfo(logTextJson)
+}
+
+module.exports = log4jsMiddleware
