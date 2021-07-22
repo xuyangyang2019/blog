8bdfa8f477397412adca925a6b23491ec5c7ae07
xuyangyang2019 <15268296911@163.com>
2021-07-22 17:45:58 +0800

    修改日志样式

--- server/middlewares/logger.js
+++ server/middlewares/logger.js
@@ -24,24 +24,19 @@ function isMobile(userAgent) {
 
 const log4jsMiddleware = async (ctx, next) => {
   const start = new Date() // 响应开始时间
+  const ip = getClientIp(ctx) // 请求的ip
+  const userAgent = isMobile(ctx.request.headers['user-agent']) // 电脑还是手机
+  const referer = ctx.request.headers['referer'] // 推荐人
+  // const path = ctx.request.path // path
+  // const reqText = `<-- ${ip} ${userAgent} ${referer} ${ctx.request.method} ${ctx.request.url}`
+  // logInfo(reqText)
   await next()
-  const execTime = new Date() - start // 响应间隔时间
-
-  // 返回客户端信息交给logger打印
-  const logTextJson = {
-    ip: getClientIp(ctx), // ip
-    userAgent: isMobile(ctx.request.headers['user-agent']), // 电脑还是手机
-    method: ctx.request.method, // 请求方法
-    path: ctx.request.path, // 请求路径
-    // url: ctx.url,
-    // request: ctx.request,
-    // body: ctx.body,
-    referer: ctx.request.headers['referer'], // 推荐人
-    status: ctx.status,
-    responseTime: execTime + 'ms'
-  }
+  const execTime = new Date() - start + 'ms' // 响应间隔时间
+  // const rspText = `--> ${ctx.request.method} ${ctx.request.url} ${ctx.status} ${execTime}`
+  // logInfo(rspText)
+  const logText = `${ip} ${userAgent} ${referer} ${ctx.request.method} ${ctx.request.url} ${ctx.status} ${execTime}`
   // 控制台输出日志
-  logInfo(logTextJson)
+  logInfo(logText)
 }
 
 module.exports = log4jsMiddleware
