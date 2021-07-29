2efea139953cc9e400df9a543d3e3753f4986904
xuyangyang2019 <15268296911@163.com>
2021-07-29 18:20:18 +0800

    重写归档页面

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="timeLine">
-    <Loading v-if="articles.time.length === 0" />
-    <ArticleList :articleList="articles.time" />
+    <Loading v-if="articlesTime.length === 0" />
+    <ArticleList :articleList="articlesTime" />
   </div>
 </template>
 
 <script>
-import { mapActions, mapState, mapMutations } from 'vuex'
-import { searchArticle } from '../api/front'
+import { mapState } from 'vuex'
+import { searchArticle, getArticlesCount } from '../api/front'
 
 import ArticleList from '@/components/article/ArticleList'
 import Loading from '@/components/base/Loading'
@@ -21,8 +21,29 @@ export default {
   },
   mixins: [headMixin],
   beforeRouteLeave(to, from, next) {
-    this.clear()
+    // this.clear()
     next()
+  },
+  asyncData({ store, route }) {
+    const timeArr = route.params.time.match(/\d+\-\d+\-\d+/g)
+    console.log(route.params.time)
+    //   // utc时间0点起
+    const startTime = new Date(Date.parse(timeArr[0])).getTime()
+    //   // utc时间24点
+    const endTime = new Date(Date.parse(timeArr[1])).getTime() + 1000 * 60 * 60 * 24
+    return Promise.all([
+      getArticlesCount('', startTime, endTime, '').then((res) => {
+        console.log(res)
+        store.commit('SET_PAGE_ARR', res.data.count || 0)
+      })
+      // searchArticle(1, 10, route.params.searchKey).then((res) => {
+      //   console.log(res)
+      //   store.commit('SET_SEARCH_RESULTS', res.data)
+      //   store.commit('PRODUCT_BG', res.data)
+      // })
+    ]).then(() => {
+      store.commit('CHANGE_CODE', 200)
+    })
   },
   head() {
     return {
@@ -31,48 +52,30 @@ export default {
   },
   computed: {
     ...mapState({
-      articles: 'articles'
+      code: 'code',
+      articlesTime: 'articlesTime'
     })
   },
-  watch: {
-    $route() {
-      this.queryArticlesByTime()
-    }
-  },
-  created() {
-    this.queryArticlesByTime()
-  },
   methods: {
-    ...mapActions({
-      GetArticlesCount: 'GetArticlesCount',
-      timeArticles: 'TimeArticles'
-    }),
-    ...mapMutations({
-      clear: 'CLEAR_PAGE',
-      SET_ARTICLES_TIME: 'SET_ARTICLES_TIME',
-      PRODUCT_BG: 'PRODUCT_BG',
-      SET_ARTICLES_SUM: 'SET_ARTICLES_SUM',
-      SET_PAGE_ARR: 'SET_PAGE_ARR'
-    }),
     // 查询归档的文章
-    queryArticlesByTime() {
-      const timeArr = this.$route.params.time.match(/\d+\-\d+\-\d+/g)
-      // utc时间0点起
-      const startTime = new Date(Date.parse(timeArr[0])).getTime()
-      // utc时间24点
-      const endTime = new Date(Date.parse(timeArr[1])).getTime() + 1000 * 60 * 60 * 24
-      searchArticle(true, 1, 10, null, startTime, endTime).then((res) => {
-        console.log(res)
-        if (res.code === 200) {
-          const articles = res.data.list
-          const articlesCount = res.data.count
-          this.SET_ARTICLES_TIME(articles)
-          this.PRODUCT_BG(articles)
-          this.SET_ARTICLES_SUM(articlesCount)
-          this.SET_PAGE_ARR(articlesCount)
-        }
-      })
-    }
+    // queryArticlesByTime() {
+    //   const timeArr = this.$route.params.time.match(/\d+\-\d+\-\d+/g)
+    //   // utc时间0点起
+    //   const startTime = new Date(Date.parse(timeArr[0])).getTime()
+    //   // utc时间24点
+    //   const endTime = new Date(Date.parse(timeArr[1])).getTime() + 1000 * 60 * 60 * 24
+    //   searchArticle(true, 1, 10, null, startTime, endTime).then((res) => {
+    //     console.log(res)
+    //     if (res.code === 200) {
+    //       const articles = res.data.list
+    //       const articlesCount = res.data.count
+    //       this.SET_ARTICLES_TIME(articles)
+    //       this.PRODUCT_BG(articles)
+    //       this.SET_ARTICLES_SUM(articlesCount)
+    //       this.SET_PAGE_ARR(articlesCount)
+    //     }
+    //   })
+    // }
   }
 }
 </script>
