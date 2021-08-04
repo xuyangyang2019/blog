fdf7f3a78f9f6b6f28ad4254f32d2f2f88c023e3
xuyangyang2019 <15268296911@163.com>
2021-08-04 11:13:21 +0800

    删除标签的背景色

--- src/components/aside/ArticleTags.vue
+++ src/components/aside/ArticleTags.vue
@@ -1,75 +1,42 @@
 <template>
   <div class="tags">
     <h2 class="tags-header">标签</h2>
-    <div class="tags-content">
-      <ul>
-        <li v-for="(item, index) in tags" :key="index" class="tags-item">
-          <a ref="tag" href="javascript: void(0)" @click="jumpGate(item.tag)">
-            <span>{{ item.tag | changeLife }}</span>
-            <span v-if="item.num" v-text="' ' + item.num + '篇'"></span>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <ul class="tags-content">
+      <li v-for="(item, index) in tags" :key="index" class="tags-item">
+        <!-- <a ref="tag" href="javascript: void(0)" @click="jumpGate(item.tag)"> -->
+        <span>{{ item.tag | changeLife }}</span>
+        <span v-if="item.num" v-text="' ' + item.num + '篇'"></span>
+        <!-- </a> -->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex'
 import { getTags } from '../../api/front'
 
 export default {
   name: 'ArticleTags',
   data() {
     return {
-      color: [
-        '#FF9933',
-        '#663300',
-        '#CC6600',
-        '#99CC33',
-        '#CC6699',
-        '#009966',
-        '#999999',
-        '#336666',
-        '#9BBFEA',
-        '#CCCC00'
-      ] // 随机的颜色库
-    }
-  },
-  computed: {
-    ...mapState({
-      tags: 'tags' // 标签
-    })
-  },
-  watch: {
-    // tag改变重新生成背景色
-    tags() {
-      this.initBackground()
+      tags: []
     }
   },
   mounted() {
     // 获取tags
-    getTags(true).then((res) => {
-      if (res.code === 200) {
-        this.SetTags(res.data)
-      }
-    })
-  },
-  methods: {
-    ...mapMutations({
-      SetTags: 'SetTags'
-    }),
-    // 初始化背景色
-    initBackground() {
-      this.$nextTick(() => {
-        // IE10不支持refs.tag
-        if (this.$refs.tag) {
-          this.$refs.tag.forEach((item) => {
-            item.style.backgroundColor = this.color[Math.floor(Math.random() * 10)]
-          })
+    getTags(true)
+      .then((res) => {
+        if (res.code === 200) {
+          this.tags = res.data
+        } else {
+          this.tags = []
         }
       })
-    },
+      .catch(() => {
+        this.tags = []
+      })
+  },
+  methods: {
     // 跳转到标签
     jumpGate(item) {
       this.$store.commit('CHANGE_TITLE', item)
@@ -95,27 +62,21 @@ export default {
   .tags-content {
     padding: 15px;
     font-size: 14px;
-    color: #1a1a1a;
     background: #faf7f7;
+    display: flex;
+    flex-wrap: wrap;
 
-    ul {
-      display: flex;
-      flex-wrap: wrap;
-      .tags-item {
-        border-radius: 5px;
-        margin: 5px;
-        overflow: hidden;
-        a {
-          font-size: 14px;
-          display: inline-block;
-          padding: 6px 15px;
-          border-radius: 5px;
-          color: #eee;
-          transition: all ease 0.5s;
-        }
-        a:hover {
-          transform: scale(1.5);
-        }
+    .tags-item {
+      color: #1a1a1a;
+      border-radius: 5px;
+      margin: 5px;
+      overflow: hidden;
+      font-size: 14px;
+      padding: 6px 15px;
+      transition: all ease 0.5s;
+      &:hover {
+        cursor: pointer;
+        transform: scale(1.5);
       }
     }
   }
