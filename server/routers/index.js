2ce5cb087e3063145c63c60eb5b18906080d1481
xuyangyang2019 <15268296911@163.com>
2021-08-09 12:19:47 +0800

    恢复管理端页面路由

--- server/routers/index.js
+++ server/routers/index.js
@@ -6,13 +6,13 @@ const router = require('koa-router')()
 const publicApi = require('../api/public/index') // rest api
 const priviteApi = require('../api/privite/index') // rest api
 const home = require('./home') // 客户端页面路由
-// const admin = require('./admin') // 管理端页面路由
+const admin = require('./admin') // 管理端页面路由
 
 // router.use('/api', api.routes(), api.allowedMethods())
 
 router.use(publicApi.routes(), publicApi.allowedMethods())
 router.use(priviteApi.routes(), priviteApi.allowedMethods())
 router.use(home.routes(), home.allowedMethods())
-// router.use(admin.routes(), admin.allowedMethods())
+router.use(admin.routes(), admin.allowedMethods())
 
 module.exports = router
